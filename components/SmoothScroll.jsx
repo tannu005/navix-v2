5e793f291a919faca4bc0dd56cc4d33b1c1166bf
Tannu <182792388+tannu005@users.noreply.github.com>
2026-04-10 22:04:10 +0530

    Add SmoothScroll component for smooth scrolling

---
+++ components/SmoothScroll.jsx
@@ -0,0 +1,11 @@
+//  components/SmoothScroll.jsx
+"use client";
+import { ReactLenis } from '@studio-freight/react-lenis';
+
+export default function SmoothScroll({ children }) {
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+      {children}
+    </ReactLenis>
+  );
+}
