d082e23a99a88001dfe4600a901e3acc6145f0c8
Tannu <182792388+tannu005@users.noreply.github.com>
2026-04-16 23:28:38 +0530

    Change import type to import for Metadata

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
